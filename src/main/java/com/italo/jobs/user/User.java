9dd89ad1ba777b5188de07bd10a8b6dff01aa5d9
Maximilian Maierhofer <mmaierhofer@outlook.de>
2023-04-11 09:35:43 +0200

    chore: init user model

---
+++ src/main/java/com/italo/jobs/user/User.java
@@ -0,0 +1,26 @@
+package com.italo.jobs.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @NonNull
+    private String firstname;
+    @NonNull
+    private String lastname;
+    @NonNull
+    private String email;
+
+}
---
+++ src/main/java/com/italo/jobs/user/UserService.java
@@ -0,0 +1,20 @@
+package com.italo.jobs.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+}
